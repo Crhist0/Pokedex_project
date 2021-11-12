2b1356cab9de767614063d7cb37adec2262def5d
Lucas Zluhan <lszluhan@hotmail.com>
2021-11-11 21:33:45 -0300

    comentei no index

--- JS/index.js
+++ JS/index.js
@@ -1,13 +1,15 @@
-var intro = document.querySelector("#intro");
-var enter = document.querySelector("#enter");
+var intro = document.querySelector('#intro');
+var enter = document.querySelector('#enter');
+
+//teste comentario comitt
 
 let mudarDiv = () => {
-  setTimeout(() => {
-    intro.style.opacity = "0";
-    intro.style.display = "none";
-    enter.style.display = "block";
-    enter.style.opacity = "100";
-  }, 4000);
+   setTimeout(() => {
+      intro.style.opacity = '0';
+      intro.style.display = 'none';
+      enter.style.display = 'block';
+      enter.style.opacity = '100';
+   }, 4000);
 };
 
 mudarDiv();
