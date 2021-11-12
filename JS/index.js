1126ccd35847ec1949e353d8fa237beae79fcf03
Jamerson Paz <ljames666@gmail.com>
2021-11-11 21:18:58 -0300

    new commit Ljames666

---
+++ JS/index.js
@@ -0,0 +1,13 @@
+var intro = document.querySelector("#intro");
+var enter = document.querySelector("#enter");
+
+let mudarDiv = () => {
+  setTimeout(() => {
+    intro.style.opacity = "0";
+    intro.style.display = "none";
+    enter.style.display = "block";
+    enter.style.opacity = "100";
+  }, 4000);
+};
+
+mudarDiv();
